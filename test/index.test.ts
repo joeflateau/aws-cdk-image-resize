4209bdac42488f25aa276ed97af06b71ef1cec73
Joe Flateau <joe@joeflateau.net>
2022-08-15 10:20:45 -0400

    test: upgrade tests

--- test/index.test.ts
+++ test/index.test.ts
@@ -1,12 +1,15 @@
-import 'aws-cdk-lib/assert/jest';
-import * as cdk from 'aws-cdk-lib/core';
-import { ImageResize } from '../src';
+import { Template } from "aws-cdk-lib/assertions";
+import * as cdk from "aws-cdk-lib/core";
+import { ImageResize } from "../src";
 
-it('Image resize', () => {
+it("Image resize", () => {
   const app = new cdk.App();
   const stack = new cdk.Stack(app);
-  new ImageResize(stack, 'TestStack');
-  expect(stack).toHaveResource('AWS::Lambda::Function');
-  expect(stack).toHaveResource('AWS::S3::Bucket');
-  expect(stack).toHaveResource('AWS::CloudFront::Distribution');
+  new ImageResize(stack, "TestStack");
+
+  const template = Template.fromStack(stack);
+
+  template.resourceCountIs("AWS::Lambda::Function", 2);
+  template.resourceCountIs("AWS::S3::Bucket", 1);
+  template.resourceCountIs("AWS::CloudFront::Distribution", 1);
 });
